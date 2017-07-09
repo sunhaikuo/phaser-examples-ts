b87011cdce394adf60f4d30d7070012ae945bc64
sunhaikuo <sunhaikuo@163.com>
2017-07-09 21:35:28 +0800

    完善

---
+++ lottery/assets/script.js
@@ -0,0 +1,43 @@
+$(function () {
+    var $input = $('.input')
+    $input.on('click', function () {
+        for (let i = 0; i < $input.length; i++) {
+            var $obj = $input.eq(i)
+            if ($obj.val() == '') {
+                $('.bd').eq(i).show()
+            }
+        }
+        var cid = $(this).attr('id')
+        console.log(cid)
+        $('#' + cid.substr(1, cid.lenght)).hide()
+    })
+    $input.on('blur', function () {
+        for (let i = 0; i < $input.length; i++) {
+            var $obj = $input.eq(i)
+            if ($obj.val() == '') {
+                $('.bd').eq(i).show()
+            }
+        }
+    })
+    $('#confirm').on('click', function () {
+        var xm = $('#ixm').val()
+        var lxfs = $('#ilxfs').val()
+        var dz = $('#idz').val()
+        if (xm && lxfs && dz && rewardId != -1) {
+            var param = {
+                value: rewardId,
+                name: xm,
+                no: lxfs,
+                address: dz
+            }
+            $.getJSON('http://lf.tongchuangjob.com/Moblie/moblie/saveUserInfo', param, function (data) {
+                if (data.result == 'success') {
+                    alert('您的信息保存成功，历时将有工作人员与您联系核实信息，请耐心等待！')
+                } else {
+                    alert('网络错误！')
+                }
+                console.log(data)
+            })
+        }
+    })
+})
--- lottery/index.ts
+++ lottery/index.ts
@@ -1,4 +1,57 @@
+// 用户填写用户信息时，传给接口的值，如："1913"
+let rewardId = -1
+// 用户的中奖信息，后台数据，1：10话费；2：50话费；3：手机
+let userReward = -1
+let $: any
 namespace Lottery {
+    // 拍到多少个西瓜人
+    let killCount = 0
+    // 屏幕的方向1：竖屏；一：横屏
+    let direction = '1'
+    // 设置屏幕的宽高，宽高是设计图的相同。注：该设置是了适配横屏游戏，所以值是相反的
+    let width = 1036
+    let height = 640
+    class BootState extends Phaser.State {
+        preload() {
+            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+            this.getDirection()
+            window.onorientationchange = this.getDirection
+            this.game.scale.onOrientationChange.add(function () {
+                this.getDirection()
+            }, this)
+            let game = this.game
+            Phaser.World.prototype.displayObjectUpdateTransform = function () {
+                if (direction == '1') {
+                    game.scale.setGameSize(height, width)
+                    this.x = game.camera.y + game.width;
+                    this.y = -game.camera.x;
+                    this.rotation = Phaser.Math.degToRad(Phaser.Math.wrapAngle(90));
+                } else {
+                    game.scale.setGameSize(width, height)
+                    this.x = -game.camera.x;
+                    this.y = -game.camera.y;
+                    this.rotation = 0;
+                }
+                PIXI.DisplayObject.prototype.updateTransform.call(this);
+            }
+        }
+        create() {
+            this.state.start('preload')
+        }
+        // 获取屏幕的方向1：竖屏；一：横屏
+        getDirection() {
+            switch (window.orientation) {
+                case 0:
+                case 180:
+                    direction = '1'
+                    break;
+                case -90:
+                case 90:
+                    direction = '一'
+                    break;
+            }
+        }
+    }
     class PreloadState extends Phaser.State {
         graphics: Phaser.Graphics
         processText: Phaser.Text
@@ -10,31 +63,38 @@ namespace Lottery {
             // } else {
             //     this.game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT
             // }
-            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+            // this.game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT
             // 屏幕旋转
-            window.onorientationchange = function () {
-                // _this.game.state.start('boot')
-                window.location.reload()
-            }
+            // window.onorientationchange = function () {
+            //     // _this.game.state.start('boot')
+            //     window.location.reload()
+            // }
             this.game.stage.backgroundColor = '#11e6cd'
+            this.load.script('zepton', './assets/zepto.min.js')
+            this.load.script('script', './assets/script.js')
             // 加载前期资源
             this.game.load.image('loadMan', './assets/load-man.png')
             this.game.load.image('processWrap', './assets/process-wrap.png')
             // 准备画面
             this.game.load.image('readyBg', './assets/ready-bg.png')
             this.game.load.image('readyBtn', './assets/ready-btn.png')
+            // 过渡动画
+            this.load.image('transBg', './assets/trans-bg.png')
+            this.load.image('trans1', './assets/trans-1.png')
+            this.load.image('trans2', './assets/trans-2.png')
+            this.load.image('transText', './assets/trans-text.png')
             // 开始游戏
             this.game.load.image('playBg', './assets/play-bg.png')
             this.game.load.image('playZw', './assets/play-zw.png')
             this.game.load.image('playTip', './assets/play-tip.png')
-            this.load.spritesheet('playP1', './assets/play-p1.png', 200, 320)
+            this.load.spritesheet('playP1', './assets/play-p1.png', 1968 / 10, 294)
             this.load.spritesheet('playP2', './assets/play-p2.png', 1734 / 10, 256)
-            this.load.spritesheet('playP3', './assets/play-p3.png', 1694 / 10, 256)
+            this.load.spritesheet('playP3', './assets/play-p3.png', 1740 / 10, 302)
             this.load.spritesheet('playP4', './assets/play-p4.png', 1610 / 10, 230)
-            this.load.spritesheet('playP5', './assets/play-p5.png', 1658 / 10, 284)
+            this.load.spritesheet('playP5', './assets/play-p5.png', 1504 / 9, 286)
             this.load.spritesheet('playP6', './assets/play-p6.png', 1498 / 10, 262)
             this.load.spritesheet('playP7', './assets/play-p7.png', 1942 / 10, 204)
-            this.load.spritesheet('playP8', './assets/play-p8.png', 1394 / 10, 286)
+            this.load.spritesheet('playP8', './assets/play-p8.png', 1440 / 10, 274)
             // 游戏统计
             this.game.load.image('countBg', './assets/count-bg.png')
             this.game.load.image('countClose', './assets/count-close.png')
@@ -59,10 +119,10 @@ namespace Lottery {
             this.game.load.image('resultClose', './assets/result-close.png')
             this.game.load.image('resultLearn', './assets/result-learn.png')
             this.game.load.image('resultShare', './assets/result-share.png')
-            this.game.load.image('resultSj', './assets/result-sj.png')
             this.game.load.image('result0', './assets/result-0.png')
-            this.game.load.image('result10', './assets/result-10.png')
-            this.game.load.image('result50', './assets/result-50.png')
+            this.game.load.image('result1', './assets/result-1.png')
+            this.game.load.image('result2', './assets/result-2.png')
+            this.game.load.image('result3', './assets/result-3.png')
             // 联系方式
             this.game.load.image('contactBg', './assets/contact-bg.png')
             this.game.load.image('contactClose', './assets/contact-close.png')
@@ -101,7 +161,32 @@ namespace Lottery {
             }, this)
         }
         create() {
-            this.game.state.start('count')
+            // alert($)
+            this.game.state.start('trans')
+            // this.game.state.start('lottery')
+        }
+    }
+    // 过渡动画
+    class TranState extends Phaser.State {
+        create() {
+            this.add.sprite(0, 0, 'transBg')
+
+            let trans2 = this.add.sprite(207, 92, 'trans2')
+            this.add.tween(trans2).from({
+                x: 1000,
+                y: 1000, alpha: 0
+            }, 1500, Phaser.Easing.Bounce.Out, true)
+            let _this = this
+            window.setTimeout(function () {
+                trans2.kill()
+                let trans1 = _this.add.sprite(77, 22, 'trans1')
+                _this.add.tween(trans1).from({ x: 1000, alpha: 0 }, 500, Phaser.Easing.Linear.None, true)
+                let text = _this.add.sprite(201, 223, 'transText')
+                _this.add.tween(text).from({ x: -1000 }, 500, Phaser.Easing.Linear.None, true)
+                window.setTimeout(function () {
+                    _this.state.start('ready')
+                }, 1500)
+            }, 2000)
         }
     }
     // 准备
@@ -130,7 +215,7 @@ namespace Lottery {
             this.zw.events.onInputDown.add(this.countPerson, this)
             this.add.sprite(408, 608, 'playTip')
             this.personGroup = this.add.physicsGroup(Phaser.Physics.ARCADE)
-            this.personGroup.createMultiple(10, ['playP2', 'playP3', 'playP4', 'playP5', 'playP6', 'playP7', 'playP8'])
+            this.personGroup.createMultiple(10, ['playP1', 'playP2', 'playP3', 'playP4', 'playP5', 'playP6', 'playP7', 'playP8'])
             this.personGroup.enableBody = true
             // 设置自动销毁 1.超出屏幕 2.
             this.personGroup.setAll('body.collideWorldBounds', false)
@@ -216,6 +301,11 @@ namespace Lottery {
             this.game.physics.arcade.collide(this.personGroup)
         }
         countPerson() {
+            killCount = this.personGroup.countLiving()
+            let _this = this
+            // window.setTimeout(function () {
+            //     _this.state.start('count')
+            // }, 1000)
             this.state.start('count')
         }
     }
@@ -225,12 +315,13 @@ namespace Lottery {
             this.add.sprite(0, 0, 'countBg')
             let close = this.add.sprite(659, 149, 'countClose')
             let lotter = this.add.sprite(341, 510, 'countBtn')
+            let radio = killCount > 10 ? Math.round(10 / 11 * 100) : Math.round(killCount / 11 * 100)
             close.inputEnabled = true
             close.events.onInputDown.add(this.close, this)
             lotter.inputEnabled = true
             lotter.events.onInputDown.add(this.lotter, this)
-            this.add.text(282, 345, '恭喜你共拍到了12个西瓜人！', { fill: '#fff', fontSize: 36 })
-            this.add.text(319, 392, '打败了全国90%的玩家！您获得了一次抽奖机会！', {
+            this.add.text(282, 335, '恭喜你共拍到了' + killCount + '个西瓜人！', { fill: '#fff', fontSize: 36 })
+            this.add.text(319, 392, '打败了全国 ' + radio + '% 的玩家！您获得了一次抽奖机会！', {
                 fill: '#fff', fontSize: 18
             })
         }
@@ -251,12 +342,14 @@ namespace Lottery {
         initInter: number = 100
         minInter: number = 40
         maxInter: number = 200
-        durning: number = 1000
+        durning: number = 500
         endTime: number = 0
         direction: string = 'up'
-        result: number = 4
+        result: number = 1
         start: boolean = false
+        isNet: boolean = true
         create() {
+            this.getResult()
             this.game.add.sprite(0, 0, 'lotBg')
             this.game.add.sprite(78, 43, 'lotKuang')
             this.game.add.sprite(654, 236, 'lotTip')
@@ -320,10 +413,15 @@ namespace Lottery {
                     console.log(4)
                     this.direction = 'lottery'
                 }
-                if (this.direction == 'lottery' && this.index == this.result) {
+                if (this.direction == 'lottery' && this.index == this.result && this.isNet) {
                     console.log(5)
                     this.direction = 'stop'
                     this.start = false
+                    let _this = this
+                    window.setTimeout(function () {
+                        _this.result = 0
+                        _this.state.start('result')
+                    }, 1000)
                 }
 
                 var posi = this.posiArr[this.index]
@@ -341,30 +439,96 @@ namespace Lottery {
         lottery() {
             this.start = true
         }
+        getResult() {
+            let _this = this
+            // 是否抽过奖
+            let isLottery = window.localStorage.getItem('lottery')
+            if (isLottery) {
+                _this.isNet = true
+                let arr = [1, 5]
+                let rnd = this.rnd.integerInRange(0, 1)
+                this.result = arr[rnd]
+            } else {
+                $.getJSON('http://lf.tongchuangjob.com/Moblie/moblie/getPrize', function (data) {
+                    console.log(data)
+                    _this.isNet = true
+                    // window.localStorage.setItem('lottery', "true")
+                    // alert(data)
+                    if (data.result == 'success') {
+                        let value = parseInt(data.value)
+                        let rnd = _this.rnd.integerInRange(0, 1)
+                        let tenArr = [2, 6]
+                        let fifArr = [0, 4]
+                        let phoArr = [3, 7]
+                        let zerArr = [1, 5]
+                        if (value == 1) {
+                            _this.result = tenArr[rnd]
+                        } else if (value == 2) {
+                            _this.result = fifArr[rnd]
+                        } else if (value == 3) {
+                            _this.result = phoArr[rnd]
+                        } else {
+                            _this.result = zerArr[rnd]
+                        }
+                        userReward = value
+                        rewardId = data.data
+                    } else {
+                        _this.result = 1
+                        userReward = 0
+                    }
+                })
+            }
+        }
     }
     // 抽奖结果
     class ResultState extends Phaser.State {
         create() {
             this.game.add.sprite(0, 0, 'resultBg')
             this.game.add.sprite(791, 26, 'resultShare')
-            this.game.add.sprite(659, 150, 'resultClose')
-            this.game.add.sprite(253, 482, 'resultAward')
-            this.game.add.sprite(542, 482, 'resultLearn')
-            this.game.add.sprite(385, 330, 'result0')
+            let close = this.game.add.sprite(659, 150, 'resultClose')
+            let reward = this.game.add.sprite(253, 482, 'resultAward')
+            let learn = this.game.add.sprite(542, 482, 'resultLearn')
+            if (userReward == 0 || userReward == -1) {
+                this.game.add.sprite(385, 330, 'result0')
+            } else if (userReward < 3) {
+                this.game.add.sprite(333, 342, 'result' + userReward)
+            } else {
+                this.game.add.sprite(285, 342, 'result' + userReward)
+            }
+            close.inputEnabled = true
+            close.events.onInputDown.add(this.close, this)
+            reward.inputEnabled = true
+            reward.events.onInputDown.add(this.reward, this)
+            learn.inputEnabled = true
+            learn.events.onInputDown.add(this.learn, this)
+        }
+        close() {
+            this.state.start('lottery')
+        }
+        reward() {
+            if (userReward != 0) {
+                this.state.start('contact')
+            }
+            // 重置抽奖结果
+            userReward = 0
+        }
+        learn() {
+            window.location.href = 'http://www.baidu.com'
         }
     }
     class ContactState extends Phaser.State {
         create() {
-            this.game.add.sprite(0, 0, 'contactBg')
-            this.game.add.sprite(760, 56, 'contactClose')
-            this.game.add.sprite(409, 413, 'contactConfirm')
+            $('#game').hide()
+            $('#contact').show()
         }
     }
     class Lottery extends Phaser.Game {
         constructor() {
-            super(1036, 640, Phaser.CANVAS, 'game')
-            this.state.add('boot', PreloadState)
+            super(width, height, Phaser.CANVAS, 'game')
+
+            this.state.add('boot', BootState)
             this.state.add('preload', PreloadState)
+            this.state.add('trans', TranState)
             this.state.add('ready', ReadyState)
             this.state.add('play', PlayState)
             this.state.add('count', CountState)
